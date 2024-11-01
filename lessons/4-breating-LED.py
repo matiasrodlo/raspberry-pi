9dbd625e79ca3d7250e8e7ca17fff5d7424a4538
Matías Rodlo <m@hablaqui.com>
2024-10-31 22:58:11 -0600

    breating led

---
+++ lessons/4-breating-LED.py
@@ -0,0 +1,48 @@
+# Lesson 4 Breathing LED
+
+# include <wiringPi.h>
+#include <stdio.h>
+#include <softPwm.h>
+
+#define LedPin    1
+
+int main(void)
+{
+    int i;
+
+    if(wiringPiSetup() == -1){ //when initialize wiring failed, print messageto screen
+        printf("setup wiringPi failed !");
+        return 1;
+    }
+    softPwmCreate(LedPin, 0, 100);
+
+    printf("\n");
+    printf("\n");
+    printf("========================================\n");
+    printf("|              Breath LED              |\n");
+    printf("|    ------------------------------    |\n");
+    printf("|         LED connect to GPIO0         |\n");
+    printf("|                                      |\n");
+    printf("|            Make LED breath           |\n");
+    printf("|                                      |\n");
+    printf("|                            SunFounder|\n");
+    printf("========================================\n");
+    printf("\n");
+    printf("\n");
+
+    while(1){
+        printf("Breath on\n");
+        for(i=0;i<=100;i++){
+            softPwmWrite(LedPin, i);
+            delay(20);
+        }
+        delay(1000);
+        printf("Breath off\n");
+        for(i=100;i>=0;i--){
+            softPwmWrite(LedPin, i);
+            delay(20);
+        }
+    }
+
+    return 0;
+}
